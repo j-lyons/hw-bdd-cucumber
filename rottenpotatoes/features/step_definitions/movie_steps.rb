24946c4730c1ac50f825acac8d908556c1188591
joe lyons <jlyons10@binghamton.edu>
2018-02-24 23:48:13 +0000

    part 2.2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -30,7 +30,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(", ")
+  ratings.each do |rating|
+    if uncheck
+      steps %Q{When I uncheck "ratings_#{rating}"}
+    else
+      steps %Q{When I check "ratings_#{rating}"}
+    end
+  end
+  #fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
